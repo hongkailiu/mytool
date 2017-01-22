cfc0d0fa7de162694e7062a3f1c4eeb425379f59
hongkailiu <hongkailiu@users.noreply.github.com>
2017-01-21 23:05:39 -0500

    Enable app structure

---
+++ src/main/java/tk/hongkailiu/mytool/MyTool.java
@@ -0,0 +1,14 @@
+package tk.hongkailiu.mytool;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by hongkailiu on 2017-01-21.
+ */
+@Slf4j public class MyTool {
+
+  public static void main(String[] args) {
+    log.info("test");
+  }
+
+}
