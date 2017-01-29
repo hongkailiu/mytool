d8290f6f8f242f66dd981ee71d5ec97e457fc13e
hongkailiu <hongkailiu@users.noreply.github.com>
2017-01-29 15:32:44 -0500

    Use java version of recursion in script

--- bin/findOrphansR.sh
+++ bin/findOrphansR.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #
 
-find /home/hliu/git-repo/gerrit/gerrit_testsite/git/ -type d -name "pack"  | while read line; do
-     /home/hliu/git-repo/test/mytool/build/distributions/mytool-0.0.3-SNAPSHOT/bin/start_my_tool.sh git -action findOrphans -folder ${line}
-done
+#find /home/hliu/git-repo/gerrit/gerrit_testsite/git/ -type d -name "pack"  | while read line; do
+#     /home/hliu/git-repo/test/mytool/build/distributions/mytool-0.0.3-SNAPSHOT/bin/start_my_tool.sh git -action findOrphans -folder ${line}
+#done
+
+readonly APP_HOME=$(dirname $(dirname $(readlink -f ${0})))
+readonly GIT_FOLDER="/home/hliu/git-repo/gerrit/2.12.gerrit_testsite/git"
+
+${APP_HOME}/bin/start_my_tool.sh git -action findOrphans \
+  -folder ${GIT_FOLDER} -recursive
